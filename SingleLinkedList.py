c8bcefa8b8443a65fb9da56e3e5ee0e50d9b874e
adrian.sandru <adrian.sandru.1990@gmail.com>
2021-01-12 22:02:05 +0200

    added remove methods and tests for them

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -1,6 +1,6 @@
 
 class SingleLinkedListNode(object):
-    """docstring for SingleLinkedListNode."""
+    """Implementation class for SingleLinkedListNode."""
 
     def __init__(self, value = None, next_node = None):
         self.value = value
@@ -15,13 +15,9 @@ class SingleLinkedListNode(object):
     def set_value(self, new_value):
         self.value = new_value
 
-    def __repr__(self):
-        nval = self.next and self.next.value or None
-        return f"[{self.value}:{repr(nval)}]"
-
 
 class SingleLinkedList(object):
-    """docstring for SingleLinkedList."""
+    """Implementation class for SingleLinkedList."""
 
     def __init__(self):
         self.startNode = SingleLinkedListNode()
@@ -30,6 +26,17 @@ class SingleLinkedList(object):
 
     def get_number_of_items(self):
         return self.count
+
+    def traverse_and_print_items(self):
+        """ Traverse and print all the items in the list """
+        if self.startNode.next != None:
+            current_node = self.startNode.next
+
+            while current_node.next != None:
+                print(str(current_node.value))
+                current_node = current_node.next
+
+            print(str(current_node.value))
 
     def push(self, data):
         """Appends a new value at the end of the linked list"""
@@ -65,5 +72,27 @@ class SingleLinkedList(object):
             while iteration != index:
                 current_node = current_node.next
                 iteration = iteration + 1
-                
+
             return current_node
+
+    def remove_first_item(self):
+        if self.startNode.next != None:
+            newNode = self.startNode.next
+            self.startNode.set_next(newNode.next)
+            newNode = None
+            self.count = self.count - 1
+
+    def remove_last_item(self):
+        if self.startNode.next != None:
+            current_node = self.startNode
+            previous_node = current_node
+
+            while current_node.next != None:
+                previous_node = current_node
+                current_node = current_node.next
+
+            previous_node.next = None
+            self.endNode.set_next(previous_node)
+            current_node = None
+
+            self.count = self.count - 1
--- data_structures_tests.py
+++ data_structures_tests.py
@@ -3,7 +3,7 @@ from SingleLinkedList import *
 def test_push():
     cars_menu = SingleLinkedList()
     cars_menu.push("Dacia")
-    assert(cars_menu.count == 1)
+    assert(cars_menu.get_number_of_items() == 1)
     cars_menu.push("Renault")
     cars_menu.push("Audi")
 
@@ -32,6 +32,23 @@ def test_get_last_element():
 
     print("test_get_last_element has passed!")
 
+def test_remove_first_item():
+    cars_menu = SingleLinkedList()
+    cars_menu.push("Dacia")
+    cars_menu.push("Renault")
+    cars_menu.push("Audi")
+    cars_menu.push("Jeep")
+
+    cars_menu.remove_first_item()
+    assert cars_menu. get_number_of_items() == 3
+
+    cars_menu.remove_first_item()
+    cars_menu.remove_first_item()
+    cars_menu.remove_first_item()
+    assert cars_menu. get_number_of_items() == 0
+
+    print("test_remove_first_item has passed!")
+
 def test_get_node_at_index():
     cars_menu = SingleLinkedList()
     cars_menu.push("Dacia")
@@ -43,7 +60,35 @@ def test_get_node_at_index():
     assert node.value == "Dacia"
     print("test_get_node_at_index has passed!")
 
+def test_remove_last_item():
+    cars_menu = SingleLinkedList()
+    cars_menu.push("Dacia")
+    cars_menu.push("Renault")
+    cars_menu.push("Audi")
+    cars_menu.push("Jeep")
+
+    print("Current items:")
+    cars_menu.traverse_and_print_items()
+
+    cars_menu.remove_last_item()
+    assert cars_menu. get_number_of_items() == 3
+
+    print("Current items after removing 1 element from the end:")
+    cars_menu.traverse_and_print_items()
+
+    cars_menu.remove_last_item()
+    cars_menu.remove_last_item()
+    cars_menu.remove_last_item()
+
+    print("Current items after removing 3 element from the end:")
+    cars_menu.traverse_and_print_items()
+
+    assert cars_menu. get_number_of_items() == 0
+    print("test_remove_last_item has passed!")
+
 test_push()
 test_get_first_element()
 test_get_last_element()
 test_get_node_at_index()
+test_remove_first_item()
+test_remove_last_item()
