5684c84ae0c8a0e0bc8f341fb473346e20336090
adrian.sandru <adrian.sandru.1990@gmail.com>
2021-01-12 21:09:05 +0200

    added draft from single linked list and tests

---
+++ SingleLinkedList.py
@@ -0,0 +1,55 @@
+
+class SingleLinkedListNode(object):
+    """docstring for SingleLinkedListNode."""
+
+    def __init__(self, value = None, next_node = None):
+        self.value = value
+        self.next = next_node
+
+    def get_value(self):
+        return self.value
+
+    def set_next(self, next_node):
+        self.next = next_node
+
+    def set_value(self, new_value):
+        self.value = new_value
+
+    def __repr__(self):
+        nval = self.next and self.next.value or None
+        return f"[{self.value}:{repr(nval)}]"
+
+
+class SingleLinkedList(object):
+    """docstring for SingleLinkedList."""
+
+    def __init__(self):
+        self.startNode = SingleLinkedListNode()
+        self.endNode = SingleLinkedListNode()
+        self.count = 0
+
+    def get_number_of_items(self):
+        return self.count
+
+    def push(self, data):
+        """Appends a new value at the end of the linked list"""
+
+        newNode = SingleLinkedListNode(data)
+        if self.startNode.next == None:
+            self.startNode.set_next(newNode)
+            self.endNode.set_next(newNode)
+        else:
+            current_node = self.startNode
+            while current_node.next != None:
+                current_node = current_node.next
+
+            current_node.set_next(newNode)
+            self.endNode.set_next(newNode)
+
+        self.count = self.count + 1
+
+    def get_first_item(self):
+        return self.startNode.next
+
+    def get_last_item(self):
+        return self.endNode.next
---
+++ data_structures_tests.py
@@ -0,0 +1,37 @@
+from SingleLinkedList import *
+
+def test_push():
+    cars_menu = SingleLinkedList()
+    cars_menu.push("Dacia")
+    assert(cars_menu.count == 1)
+    cars_menu.push("Renault")
+    cars_menu.push("Audi")
+
+    assert(cars_menu.get_number_of_items() == 3)
+    print("test_push has passed!")
+
+def test_get_first_element():
+        cars_menu = SingleLinkedList()
+        cars_menu.push("Dacia")
+        cars_menu.push("Renault")
+        cars_menu.push("Audi")
+
+        first_element = cars_menu.get_first_item()
+        assert first_element.value == "Dacia"
+
+        print("test_get_first_element has passed!")
+
+def test_get_last_element():
+        cars_menu = SingleLinkedList()
+        cars_menu.push("Dacia")
+        cars_menu.push("Renault")
+        cars_menu.push("Audi")
+
+        first_element = cars_menu.get_last_item()
+        assert first_element.value == "Audi"
+
+        print("test_get_last_element has passed!")
+
+test_push()
+test_get_first_element()
+test_get_last_element()
